feb921aaa82d7e6a23b7afbd21b251d4291913fd
Belindaliang51 <belinda.liang51@gmail.com>
2021-01-11 08:07:36 -0500

    Upload files

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Footer(){
+    const currentYear = new Date().getFullYear();
+
+    return <footer>copyright &copy;	{currentYear} demo _liang</footer>
+}
+
+export default Footer;
