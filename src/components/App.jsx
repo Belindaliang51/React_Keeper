ddd4990a50d9b8a8e4fc0a6ad8bc1e862e66238f
Belindaliang51 <belinda.liang51@gmail.com>
2021-01-26 07:51:47 -0500

    Edit package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
+import Note from "./Note"
+import notes from "../notes";
+
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
-            <Footer />
         </div>
-    );
+    )
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+
 
 ReactDOM.render(
     <App />,
